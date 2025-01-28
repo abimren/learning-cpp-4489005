b4d4fb8113c0bc0370c7b2e2060d3660e0634d52
Abdurrahman Imren <90402516+abimren@users.noreply.github.com>
2025-01-28 21:39:06 +0000

    preprocessor define debug

--- src/Ch02/02_06b/CodeDemo.cpp
+++ src/Ch02/02_06b/CodeDemo.cpp
@@ -3,9 +3,21 @@
 // Preprocessor directives, by Eduardo Corpeño 
 
 #include <iostream>
+#include <cstdint>
+#include <string>
+
+#define CAPA 5000
+#define DEBUG
 
 int main(){
-    
+    int32_t large = CAPA;
+    uint8_t small = 37;
+#ifdef DEBUG
+    std::cout << "[Dut dut we are gonna Dut]"<<std::endl;
+#endif
+    large += small; // large = large + small;
+    std::cout << "The large is "<<large<<std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
