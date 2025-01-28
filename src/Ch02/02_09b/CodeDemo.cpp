e3cd642b6029e804b1b3e20b713814369680d756
Abdurrahman Imren <90402516+abimren@users.noreply.github.com>
2025-01-28 22:23:59 +0000

    string class concatenation

--- src/Ch02/02_09b/CodeDemo.cpp
+++ src/Ch02/02_09b/CodeDemo.cpp
@@ -3,9 +3,21 @@
 // Strings, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string>
+#include <cstring>
 
 int main(){
-    
+    const size_t LENGTH1 = 25;
+    char arr_str1[LENGTH1]="Hey Keko";
+    char arr_str2[]="Whats up";
+
+    std::string std_str1 = "Standard kekko";
+    std::string std_str2 = "What happened kekko";
+
+    strncat(arr_str1,arr_str2,LENGTH1);
+    std::cout << arr_str1 << std::endl;
+    std::cout << std_str1 + std_str2 << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
