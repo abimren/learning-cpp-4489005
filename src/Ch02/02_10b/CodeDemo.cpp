2b775d280d9c7c21268cde592786f7729094d1f3
Abdurrahman Imren <90402516+abimren@users.noreply.github.com>
2025-01-28 22:37:42 +0000

    type casting

--- src/Ch02/02_10b/CodeDemo.cpp
+++ src/Ch02/02_10b/CodeDemo.cpp
@@ -6,7 +6,19 @@
 #include <cstdint>
 
 int main(){
-    
+    float flt;
+    int32_t sgn;
+    uint32_t usgn;
+
+    flt = -3.25;
+    sgn = flt;
+    usgn = sgn; 
+
+    std::cout << "float " << flt << std::endl;
+    std::cout << "sgn " << sgn << std::endl;
+    std::cout << "usgn " << usgn << std::endl; //2^32+sgn
+    std::cout << "usgn (int32_t) " << (int32_t) usgn << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
