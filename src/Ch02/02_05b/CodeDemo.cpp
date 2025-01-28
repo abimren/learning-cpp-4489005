8436617c8c6c3430093d43480afc10b7a13cb2f2
Abdurrahman Imren <90402516+abimren@users.noreply.github.com>
2025-01-28 21:15:31 +0000

    using auto type inference

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -6,6 +6,21 @@
 #include <typeinfo>
 
 int main(){
+    auto a=5;
+    auto b=12345678901;
+    auto c=3.15f;
+    auto d=3.15;
+    auto e=true;
+    auto f='d';
+    auto g="C++ keks :)";
+    //std::cout << "The type of a is " << typeid(a) << std::endl;
+    std::cout <<"Type of a is "<<typeid(a).name()<<std::endl;
+    std::cout <<"Type of b is "<<typeid(b).name()<<std::endl;
+    std::cout <<"Type of c is "<<typeid(c).name()<<std::endl;
+    std::cout <<"Type of d is "<<typeid(d).name()<<std::endl;
+    std::cout <<"Type of e is "<<typeid(e).name()<<std::endl;
+    std::cout <<"Type of f is "<<typeid(f).name()<<std::endl;
+    std::cout <<"Type of g is "<<typeid(g).name()<<std::endl;
     
     std::cout << std::endl << std::endl;
     return (0);
