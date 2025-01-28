48c68a5a4cbfd8b7a2550da696880ffc8fd57416
Abdurrahman Imren <90402516+abimren@users.noreply.github.com>
2025-01-28 20:55:57 +0000

    variables

--- src/Ch02/02_03b/CodeDemo.cpp
+++ src/Ch02/02_03b/CodeDemo.cpp
@@ -4,9 +4,26 @@
 
 #include <iostream>
 
+int a, b=5; // define globally and b initialization
+/*
+Code describe
+multi line*/
 int main(){
-    std::cout << "Hi There!" << std::endl;
-    
+    bool kekos_flag;
+    a=7;
+    kekos_flag=false;
+    std::cout << "a= " << a << std::endl;
+    std::cout << "b= " << b << std::endl;
+    std::cout << "kekos= " << kekos_flag << std::endl;
+    kekos_flag=true;
+
+    std::cout << "kekos= " << kekos_flag << std::endl;
+    std::cout << "a+b= " << a + b << std::endl;
+    std::cout << "b-a= " << b - a << std::endl;
+    unsigned int posit;
+    posit = b-a;
+    std::cout << "b-a (posit)= " << posit << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
