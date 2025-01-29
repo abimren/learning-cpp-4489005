1dfc8e8a6706fdfd7a9a02a5cff63e585f72c5c8
Abdurrahman Imren <90402516+abimren@users.noreply.github.com>
2025-01-29 12:30:44 +0000

    while loop

--- src/Ch04/04_03b/CodeDemo.cpp
+++ src/Ch04/04_03b/CodeDemo.cpp
@@ -8,8 +8,22 @@
 int main(){
     std::vector<int> numbers = {12, 25, 31, 47, 58};
     
-    std::vector<int>::iterator ptr = numbers.begin();
+    //std::vector<int>::iterator ptr = numbers.begin();
+    auto ptr = numbers.begin();
     
+    while(ptr != numbers.end()){
+        std::cout<<"Num: "<<*ptr<<" ";
+        ptr=next(ptr,1); //to move iterator by 1 position
+    }
+    std::cout << std::endl;
+    int i=0;
+    do
+    {
+        std::cout<<"Num: "<<numbers[i]<<" ";
+        i++;
+    } while (i<numbers.size());
+    
+
     std::cout << std::endl << std::endl;
     return (0);
 }
