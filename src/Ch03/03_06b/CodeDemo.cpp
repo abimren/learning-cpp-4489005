6f1728025749f0b816839a85f0d560dff0d76a90
Abdurrahman Imren <90402516+abimren@users.noreply.github.com>
2025-01-29 02:21:12 +0000

    pointers begin

--- src/Ch03/03_06b/CodeDemo.cpp
+++ src/Ch03/03_06b/CodeDemo.cpp
@@ -6,6 +6,14 @@
 #include <string>
 
 int main(){
+    int a=31;
+    int *ptr;
+    ptr=&a;
+    std::cout<<"Content of a is "<<a<<std::endl;
+    std::cout<<"ptr is pointing address "<<ptr<<std::endl;
+    std::cout<<"address of a is "<<&a<<std::endl;
+    std::cout<<"ptr points to "<<*ptr<<std::endl;
+    std::cout<<"address of ptr is "<<&ptr<<std::endl;
 
     std::cout << std::endl << std::endl;
     return (0);
