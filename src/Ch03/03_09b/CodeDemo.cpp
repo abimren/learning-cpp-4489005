af9f6cbabdec0f0f3b27d05a3dd9f882c4613064
Abdurrahman Imren <90402516+abimren@users.noreply.github.com>
2025-01-29 02:46:05 +0000

    vectors

--- src/Ch03/03_09b/CodeDemo.cpp
+++ src/Ch03/03_09b/CodeDemo.cpp
@@ -2,11 +2,21 @@
 // Exercise 03_09
 // Vectors, by Eduardo Corpeño 
 
+#include <vector>
 #include <iostream>
 #include <string>
 
 int main(){
-    
+    std::vector<int> primes;
+    primes.push_back(2);
+    primes.push_back(4);
+    primes.push_back(6);
+    primes.push_back(0);
+    std::cout << "vector has " << primes.size() << " elements."<< std::endl;
+    std::cout << "Index [2] is " << primes[2] << " ."<< std::endl;
+    primes[2]=31;
+    std::cout << "New index [2] is " << primes[2] << " ."<< std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
