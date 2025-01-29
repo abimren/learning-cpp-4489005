c8c83f85e8e63212945af3d3855ed808490ba43d
Abdurrahman Imren <90402516+abimren@users.noreply.github.com>
2025-01-29 00:49:18 +0000

    struct

--- src/Ch03/03_01b/CodeDemo.cpp
+++ src/Ch03/03_01b/CodeDemo.cpp
@@ -5,8 +5,22 @@
 #include <iostream>
 #include <string>
 
+enum class cow_purpose {dairy, meat, hide, pet};
+struct cow{
+    std::string name;
+    int age;
+    cow_purpose purpose;
+};
+
 int main(){
-    
+    cow my_beef;
+    my_beef.age=3;
+    my_beef.name="steak";
+    my_beef.purpose=cow_purpose::meat;
+
+    std::cout << my_beef.name << " is a type "<< (int) my_beef.purpose <<" beef."<<std::endl;
+    std::cout << my_beef.name << " is "<< my_beef.age <<" years old."<<std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
