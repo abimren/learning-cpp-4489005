718ae6122e7f590ba6b7055bb2e355e62e67e794
Abdurrahman Imren <90402516+abimren@users.noreply.github.com>
2025-01-29 03:13:10 +0000

    vector ops

--- src/Ch03/03_10b/CodeDemo.cpp
+++ src/Ch03/03_10b/CodeDemo.cpp
@@ -8,6 +8,17 @@
 #include "cow.h"
 
 int main(){
+    std::vector<cow> cattle;
+    
+    cattle.push_back(cow("Maca",35,cow_purpose::hide));
+    cattle.push_back(cow("Dana",35,cow_purpose::dairy));
+    cattle.push_back(cow("Itsy",30,cow_purpose::pet));
+    cattle.push_back(cow("Pitsy",25,cow_purpose::pet));
+
+    std::cout << "The first cow is " << cattle.begin()->get_name() << std::endl;
+    std::cout << "At index 1 we have " << cattle[1].get_name() << std::endl;
+    std::cout << "Next to last is " << prev(cattle.end(), 2)->get_name() << std::endl;
+    std::cout << "The last cow is " << (cattle.end() - 1)->get_name() << std::endl;
     
     std::cout << std::endl << std::endl;
     return (0);
